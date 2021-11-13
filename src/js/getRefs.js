eb1c66835b88be195bc1a548f8e3e15abf930624
Svit-l <lov.svit@gmail.com>
2021-11-13 15:28:05 +0200

    task1

---
+++ src/js/getRefs.js
@@ -0,0 +1,8 @@
+export const getRefs = () => {
+  return {
+    temperDegree: document.querySelector('.temperature-degree'),
+    locationTimezone: document.querySelector('.location-timezone'),
+    temperatureDescription: document.querySelector('.temperature-description'),
+    icon: document.querySelector('.icon'),
+  };
+};
