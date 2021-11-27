8c9765927d551273158e57ea64a499964feb855f
Kretsul-Vitaliy <kreculvitalii@gmail.com>
2021-11-27 17:01:46 +0200

    add time

--- src/js/date-time.js
+++ src/js/date-time.js
@@ -1,7 +1,15 @@
 import { format, compareAsc } from 'date-fns';
-
+import { getRefs } from "./getRefs";
+const refs = getRefs();
 format(new Date(), 'do');
-console.log(format(new Date(), 'do'));
-console.log(format(new Date(), 'EEE'));
-console.log(format(new Date(), 'MMMM'));
-console.log(format(new Date(), 'hh:mm:ss'));
+// console.log(format(new Date(), 'do'));
+// console.log(format(new Date(), 'EEE'));
+// console.log(format(new Date(), 'MMMM'));
+// console.log(format(new Date(), 'hh:mm:ss'));
+refs.day.textContent = format(new Date(), 'do');
+refs.weekDay.textContent = format(new Date(), 'EEE');
+refs.month.textContent = format(new Date(), 'MMMM');
+setInterval(() => {
+  refs.time.textContent = format(new Date(), 'hh:mm:ss');  
+}, 1000);
+console.log(refs.month);
